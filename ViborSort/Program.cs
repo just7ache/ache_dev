6a52f77cea92aca0e541544444b8edc54fa79d45
Tyuiu.ChebulaevAG@outlook.com <andrey.chebulaev@gmail.com>
2024-06-09 12:50:32 +0500

    upd1

---
+++ ViborSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViborSort
+{
+    internal class Program
+    {
+        static void ViborSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во чисел в массиве");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите элементы массива");
+            int[] array = new int[N];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            ViborSort(array);
+            Console.WriteLine("Сортировка выбором");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sort2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VstavkiSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 7, 0, -4, 3, 1, -2, 5 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int current_elem = array[i];
+                int prev_elem = i - 1;
+
+                while (prev_elem >= 0 && array[prev_elem] > current_elem)
+                {
+                    array[prev_elem + 1] = array[prev_elem];
+                    array[prev_elem] = current_elem;
+                    prev_elem--;
+                }
+            }
+
+            Console.WriteLine("Сортировка вставками");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
