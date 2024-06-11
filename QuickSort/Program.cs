2d30b6abcb90dc8eedc6556a3f997d1235f6b0a2
Tyuiu.ChebulaevAG@outlook.com <andrey.chebulaev@gmail.com>
2024-06-11 16:06:13 +0500

    upd2

---
+++ QuickSort/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    internal class Program
+    {
+        static void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivorIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivorIndex - 1);
+            QuickSort(array, pivorIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        static int[] QuickSort(int[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            var len = Convert.ToInt32(Console.ReadLine());
+            var a = new int[len];
+
+            for (var i = 0; i < a.Length; ++i)
+            {
+                Console.Write("a[{0}] = ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Готовый массив (быстрая сортировка): {0}", string.Join(", ", QuickSort(a)));
+
+            Console.ReadLine();
+        }
+    }
+}
